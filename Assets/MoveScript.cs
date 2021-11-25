bb176a2d25b4e465262d894ca7a55fa542f2edf8
Robert Denomme <denommenator@gmail.com>
2021-11-24 21:41:39 -0500

    Added multiple time steps per Update, Collision detection/resolution and gravity

--- Assets/MoveScript.cs
+++ Assets/MoveScript.cs
@@ -8,11 +8,12 @@ public class MoveScript : MonoBehaviour
 
     ComputeBuffer positionBuffer, nextPositionBuffer;
     ComputeBuffer velocityBuffer, nextVelocityBuffer;
-    ComputeBuffer accelerationBuffer, nextAccelerationBuffer;
 
     ComputeBuffer fixedPointIterationPositionBuffer, fixedPointIterationVelocityBuffer;
 
     ComputeBuffer densityBuffer, pressureBuffer;
+
+
 
     [SerializeField, Range(0, 1_000)]
     public float spiralScale = 10;
@@ -21,15 +22,19 @@ public class MoveScript : MonoBehaviour
     private int nBodies;
 
     [SerializeField, Range(1, 40)]
-    private uint fixedPointIterations = 1;
+    private int stepsPerUpdate = 1;
+
+    [SerializeField, Range(1, 40)]
+    private int fixedPointIterations = 1;
+
+    [SerializeField, Range(0, 1)]
+    float wall_elasticity = 1.0f;
 
     [SerializeField]
     Material material;
 
     [SerializeField]
     Mesh mesh;
-
-
 
     [SerializeField]
     ComputeShader moveShader;
@@ -40,11 +45,14 @@ public class MoveScript : MonoBehaviour
     static readonly int velocitiesID = Shader.PropertyToID("_Velocities");
     static readonly int nextVelocitiesID = Shader.PropertyToID("_NextVelocities");
 
+    static readonly int fixedPointIterationsPositionsID = Shader.PropertyToID("_FixedPointIterationPositions");
+    static readonly int fixedPointIterationsVelocitiesID = Shader.PropertyToID("_FixedPointIterationVelocities");
+
+
     static readonly int copyBufferOriginID = Shader.PropertyToID("_CopyBufferOrigin");
     static readonly int copyBufferDestinationID = Shader.PropertyToID("_CopyBufferDestination");
 
-    static readonly int fixedPointIterationsPositionsID = Shader.PropertyToID("_FixedPointIterationPositions");
-    static readonly int fixedPointIterationsVelocitiesID = Shader.PropertyToID("_FixedPointIterationVelocities");
+    
 
     static readonly int densitiesID = Shader.PropertyToID("_Densities");
     static readonly int pressuresID = Shader.PropertyToID("_Pressures");
@@ -52,21 +60,86 @@ public class MoveScript : MonoBehaviour
     static readonly int deltaTimeID = Shader.PropertyToID("_DeltaTime");
     static readonly int nBodiesID = Shader.PropertyToID("_nBodies");
 
-    private float containerSize;
+    static readonly int collisionContainerWallsID = Shader.PropertyToID("_CollisionContainerWalls");
+    static readonly int nCollisionContainerWallsID = Shader.PropertyToID("_nCollisionContainerWalls");
+
+    static readonly int currentWallID = Shader.PropertyToID("_CurrentWallID");
 
     
 
-    List<GameObject> bodies;
 
-    void OnEnable()
+
+
+    private float containerSize;
+
+    //refactor to 3 buffers
+    public struct CollisionContainerWall
+    {
+        public Vector3 inward_normal;
+        public Vector3 point;
+        public float elasticity;
+    }
+
+    private int nCollisionContainerWalls;
+
+    private CollisionContainerWall[] collisionContainerWalls;
+    ComputeBuffer CollisionContainerWallBuffer;
+
+    CollisionContainerWall[] BoxContainer(float containerSize)
+    {
+        CollisionContainerWall wall_lower, wall_upper, wall_left, wall_right;
+
+        wall_lower = new CollisionContainerWall
+            {
+                inward_normal = Vector3.up,
+                point = containerSize / 2 * Vector3.down,
+                elasticity = wall_elasticity
+            };
+
+        wall_upper = new CollisionContainerWall
+        {
+            inward_normal = Vector3.down,
+            point = containerSize / 2 * Vector3.up,
+            elasticity = wall_elasticity
+        };
+
+        wall_left = new CollisionContainerWall
+        {
+            inward_normal = Vector3.right,
+            point = containerSize / 2 * Vector3.left,
+            elasticity = wall_elasticity
+        };
+
+        wall_right = new CollisionContainerWall
+        {
+            inward_normal = Vector3.left,
+            point = containerSize / 2 * Vector3.right,
+            elasticity = wall_elasticity
+        };
+
+
+        CollisionContainerWall[] walls = new CollisionContainerWall[]
+            { wall_lower, wall_upper, wall_left, wall_right };
+
+        return walls;
+
+    }
+
+
+    void Start()
     {
         nBodies = Mathf.FloorToInt(1.0f * Mathf.PI * Mathf.Pow(spiralScale, 2));
 
         containerSize = spiralScale * Mathf.Sqrt(nBodies);
+        nCollisionContainerWalls = 5;
+
+        //allocate & initialize buffers
 
         densityBuffer = new ComputeBuffer(nBodies, sizeof(float));
         pressureBuffer = new ComputeBuffer(nBodies, sizeof(float));
 
+
+        //initial positions & velocities
         positionBuffer = new ComputeBuffer(nBodies, 3 * sizeof(float));
         nextPositionBuffer = new ComputeBuffer(nBodies, 3 * sizeof(float));
 
@@ -92,9 +165,17 @@ public class MoveScript : MonoBehaviour
 
         velocityBuffer.SetData(zero_vectors);
 
+        //collision container
+
+        collisionContainerWalls = BoxContainer(containerSize);
+        //
+        CollisionContainerWallBuffer = new ComputeBuffer(nCollisionContainerWalls, sizeof(float) * (3 * 2 + 1));
+        CollisionContainerWallBuffer.SetData(collisionContainerWalls);
+
+
     }
 
-    void OnDisable()
+    void OnDestroy()
     {
         positionBuffer.Release();
         nextPositionBuffer.Release();
@@ -107,6 +188,8 @@ public class MoveScript : MonoBehaviour
 
         densityBuffer.Release();
         pressureBuffer.Release();
+
+        CollisionContainerWallBuffer.Release();
 
     }
 
@@ -127,15 +210,15 @@ public class MoveScript : MonoBehaviour
 
 
 
-    // Start is called before the first frame update
-    void Start()
+
+    void FixedUpdate()
     {
 
+        for (int step = 0; step < stepsPerUpdate; step++)
+            StepSimulation();
     }
 
-
-    
-    void FixedUpdate()
+    void StepSimulation()
     {
         moveShader.SetInt(nBodiesID, nBodies);
 
@@ -169,7 +252,7 @@ public class MoveScript : MonoBehaviour
             moveShader.Dispatch(moveShader.FindKernel("ComputePressure"), nBodies, 1, 1);
 
             //compute next fixed point iteration
-            moveShader.SetFloat(deltaTimeID, Time.fixedDeltaTime / fixedPointIterations);
+            moveShader.SetFloat(deltaTimeID, Time.fixedDeltaTime / stepsPerUpdate);
 
 
             moveShader.SetBuffer(moveShader.FindKernel("FixedPointIteration"), densitiesID, densityBuffer);
@@ -197,6 +280,19 @@ public class MoveScript : MonoBehaviour
 
             moveShader.Dispatch(moveShader.FindKernel("CopyBuffers"), nBodies, 1, 1);
         }
+
+        //collision detection / resolution
+        moveShader.SetInt(nCollisionContainerWallsID, nCollisionContainerWalls);
+        moveShader.SetBuffer(moveShader.FindKernel("ResolveWallCollisions"), nextPositionsID, nextPositionBuffer);
+        moveShader.SetBuffer(moveShader.FindKernel("ResolveWallCollisions"), nextVelocitiesID, nextVelocityBuffer);
+        moveShader.SetBuffer(moveShader.FindKernel("ResolveWallCollisions"), collisionContainerWallsID, CollisionContainerWallBuffer);
+        for (int wall_id = 0; wall_id < nCollisionContainerWalls; wall_id++)
+        {
+            moveShader.SetInt(currentWallID, wall_id);
+            moveShader.Dispatch(moveShader.FindKernel("ResolveWallCollisions"), nBodies, 1, 1);
+        }
+        
+
 
 
         //copy data from next position/velocity to current position/velocity
