2b89586ab1406b3886ba4bc372e4263e5d619015
Robert Denomme <denommenator@gmail.com>
2021-12-17 17:25:16 -0500

    Small tweak to Density test. Tested 10_000 particles, Apple M1 GPU didn't get up to full usage.

--- Assets/Tests/PlayMode/SPH/SPHDensityTests.cs
+++ Assets/Tests/PlayMode/SPH/SPHDensityTests.cs
@@ -44,19 +44,19 @@ namespace SPH
         [Test]
         public void SmallCubeTest()
         {
-            int NGrid = 10;
+            int NGrid_x = 10, NGrid_y = 10, NGrid_z = 100;
             float delta = 1.0f;
 
             List<Vector3> Positions = new List<Vector3>();
 
-            for (int i = 0; i < NGrid; i++)
-                for (int j = 0; j < NGrid; j++)
-                    for (int k = 0; k < NGrid; k++)
+            for (int i = 0; i < NGrid_x; i++)
+                for (int j = 0; j < NGrid_y; j++)
+                    for (int k = 0; k < NGrid_z; k++)
                         Positions.Add(new Vector3(i * delta, j * delta, k * delta));
 
-            float[] Densities = densityKernel.ComputeDensity(Positions.ToArray(), 1, NGrid * NGrid * NGrid);
+            float[] Densities = densityKernel.ComputeDensity(Positions.ToArray(), 1, Positions.Count);
 
-            Debug.Log("Average Density: " + Enumerable.Average(Densities));
+            Debug.Log(Positions.Count + "Particles. Average Density: " + Enumerable.Average(Densities));
             for (int p_id = 0; p_id < Densities.Length; p_id++)
             {
                 Debug.Log(Densities[p_id].ToString());
